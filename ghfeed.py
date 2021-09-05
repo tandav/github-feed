0befdcd45f575abb8f4b4a583831a1cb824ab5f4
Alexander Rodionov <alx1dr@gmail.com>
2021-09-05 22:03:58 +0300

    add auth and better progressbar

--- ghfeed.py
+++ ghfeed.py
@@ -7,6 +7,7 @@ import tqdm
 from collections.abc import Iterable
 from typing import Union
 
+from credentials import user_token
 import pipe21 as P
 
 
@@ -17,25 +18,29 @@ def get_following(
 ) -> list:
     users = []
     for page in pages:
-        p = {'per_page': 100, 'page': page}
-        for user in requests.get(f'https://api.github.com/users/{user}/following', params=p).json():
-            users.append(tuple(user[field] for field in fields))
+        for followed_user in requests.get(
+            f'https://api.github.com/users/{user}/following',
+            params={'per_page': 100, 'page': page},
+            auth=user_token,
+        ).json():
+            users.append(tuple(followed_user[field] for field in fields))
     return users
 
 
 def get_events(user: Union[str, Iterable]) -> list:
     if isinstance(user, str): # one user
-        print(user)
         return (
             requests
-            .get(f'https://api.github.com/users/{user}/events')
+            .get(f'https://api.github.com/users/{user}/events', auth=user_token)
             .json()
             | P.Map(lambda e: (e['created_at'].split('T')[0], user, e['repo']['name'], e['type']))
             | P.Pipe(list)
         )
     elif isinstance(user, Iterable):
         events = []
-        for u in tqdm.tqdm(user):
+        t = tqdm.tqdm(user)
+        for u in t:
+            t.set_description(u)
             events += get_events(u)
         return events
         #return user | P.FlatMap(get_events) | P.Pipe(sorted)
