d3791cb5d4889ca3a1771e36371deda163ca6049
Alexander Rodionov <alx1dr@gmail.com>
2021-09-16 11:16:37 +0300

    move update_html to server.py

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from make_html import main as make_html
 from pathlib import Path
+import concurrent.futures
+import time
+
+
+def update_html():
+    while True:
+        make_html()
+        time.sleep(60)
+
+
+with concurrent.futures.ThreadPoolExecutor() as pool:
+    pool.submit(update_html)
+
 
 html = Path('index.html')
 app = FastAPI()
--- sheduler.py
+++
@@ -1,6 +0,0 @@
-import time
-from make_html import main as make_html
-
-while True:
-    make_html()
-    time.sleep(60)
